67c2eac38d1a04a88fb86b661fc708407af8e6d1
Rena Goldsmith <rena.kg@gmail.com>
2016-07-12 00:14:39 +1200

    called weather API

--- components/Weather.js
+++ components/Weather.js
@@ -1,12 +1,50 @@
-import React from 'react'
+import React, { Component } from 'react'
+import request from 'superagent'
 
-export default React.createClass({
+class Weather extends Component {
+    constructor(props){
+        super(props)
+        this.state = {}
+    }
+
+    componentWillMount() {
+          const url = `${YAHOO_WHAKATANE_WEATHER}`
+          request.get(url)
+            .then((response) => {
+                const results = response.body.query
+                this.setState=(
+                    {   location: results }
+                )
+                console.log(this.setState)
+            })
+            .catch((err) => console.error(err))
+    }
+
+    componentDidMount() {
+
+    }
+
+    componentWillReceiveProps(nextProps) {
+
+    }
+
+    componentWillUpdate(nextProps, nextState) {
+
+    }
+
+    componentWillUnmount() {
+
+    }
+
     render() {
         return (
             <div id="weather">
                 <h2>Huarere ki Hikurangi</h2>
                 <h3>Weather - Five day outlook</h3>
-            </div>
+                <p></p>
+             </div>
         )
     }
-})
+}
+
+export default Weather
