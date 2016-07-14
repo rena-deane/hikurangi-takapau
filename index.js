bfe5b39ee2be6d0efa3ac4c01a01a0c40079a9f5
Rena Goldsmith <rena.kg@gmail.com>
2016-07-14 22:23:28 +1200

    Updates

---
+++ index.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import { render } from 'react-dom'
+import { Router, Route, hashHistory } from 'react-router'
+import App from './client/components/App'
+import About from './client/components/About'
+import Carvings from './client/components/Carvings'
+import Weather from './client/components/Weather'
+import LandingPage from './client/components/LandingPage'
+
+render((
+    <Router history={hashHistory}>
+        <Route path="/" component={App}>
+            <Route path="/explore" component={Carvings} />
+            <Route path="/about" component={About} />
+            <Route path="/weather" component={Weather} />
+        </Route>
+    </Router>
+), document.querySelector('main'))
+console.log('welcome to hikurangi-takapau')
