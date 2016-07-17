c91a896399838098e9ffe400a65afa4ceda79963
Rena Goldsmith <rena.kg@gmail.com>
2016-07-17 22:48:18 +1200

    resize carvings image

--- client/components/Weather.js
+++ client/components/Weather.js
@@ -39,7 +39,7 @@ class Weather extends Component {
             <div id="weather">
                 <h3>Current Conditions</h3>
                 {
-                    this.state.isFetching ? <img className='loading' src='./images/triangle.gif' />
+                    this.state.isFetching ? <img className='loading' src='../public/images/triangle.gif' />
                     : <Current
                         day={current.day}
                         date={current.date}
@@ -55,9 +55,9 @@ class Weather extends Component {
                         sunset={this.state.astronomy.sunrise} />
                 }
                 <h3>Five-Day Forecast</h3>
-                <div className="show-forecast">
+                <div id="show-forecast">
                 {
-                    this.state.isFetching ? <img className='loading' src='./images/triangle.gif' />
+                    this.state.isFetching ? <img className='loading' src='../public/images/triangle.gif' />
                     : forecast.map( (fore, index) => {
                         return <Forecast
                                     key={index}
